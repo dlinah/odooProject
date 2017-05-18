80df570939e6352cd8e32ceeb36d900121994a90
salamaashoush <salamaashoush@gmail.com>
2017-05-18 15:16:53 +0200

    code refactoring

--- models/product.py
+++ models/product.py
@@ -1,49 +1,43 @@
-from odoo import models, fields ,api
-from scraper import souqScrapper
+from odoo import models, fields, api
+from scraper import SouqScrapper
 import base64
-import urllib2 
+import urllib2
+
 
 class Product(models.Model):
+    def load_image(self, image_url):
+        print 'ckdmkemdkmdkemdk --------------'
+        image = base64.decodestring(urllib2.urlopen(image_url).read())
+        print image
+        self.image = image
+        print 'inchange'
 
+    _name = 'souq.product'
+    link = fields.Char()
+    name = fields.Char()
+    price = fields.Char()
+    description = fields.Char()
+    image = fields.Binary()
+    condition = fields.Char()
+    seller = fields.Char()
 
-	def load_image(self,image_url):
-		print 'ckdmkemdkmdkemdk --------------'
-		image = base64.decodestring(urllib2.urlopen(image_url).read())
-		print image
-		self.image=image
-		print 'inchange'
+    def update_product(self):
+        scraper = SouqScrapper(self.link)
+        self.name = scraper.name()
+        self.price = scraper.price()
+        self.description = scraper.description()
+        image_url = scraper.image_url()
+        self.load_image(image_url)
+        print(self.image)
 
-
-
-	_name='souq.product'
-	link=fields.Char()
-	name=fields.Char()
-	price=fields.Char()
-	description=fields.Char()
-	image=fields.Binary()
-	condition=fields.Char()
-	seller=fields.Char()
-
-	def update_product(self):
-		scraper = souqScrapper(self.link)
-		self.name= scraper.name()
-		self.price= scraper.price()
-		self.description=scraper.description()
-		image_url=scraper.image_url()
-		self.load_image(image_url)
-		print(self.image)
-
-	def update_all_products(self):
-		records = self.env['souq.product'].search([])
-		for record in records:
-			print ("id :", record.id)
-			obj = self.env['souq.product'].browse([record.id])
-			scraper = souqScrapper(obj.link)
-			name= scraper.name()
-			price= scraper.price()
-			description=scraper.description()
-			image_url=scraper.image_url()
-			res = obj.write({"name": name, "description": description, "price": price,'image_url':image_url})
-       
-	
-    
+    def update_all_products(self):
+        records = self.env['souq.product'].search([])
+        for record in records:
+            print ("id :", record.id)
+            obj = self.env['souq.product'].browse([record.id])
+            scraper = SouqScrapper(obj.link)
+            name = scraper.name()
+            price = scraper.price()
+            description = scraper.description()
+            image_url = scraper.image_url()
+            res = obj.write({"name": name, "description": description, "price": price, 'image_url': image_url})
--- models/scraper.py
+++ models/scraper.py
@@ -1,39 +1,30 @@
-
 import requests
-import  lxml
 from bs4 import BeautifulSoup
 
 
-
-
-class souqScrapper:
-    def __init__(self , url):
-         self.source = requests.get(url)
-         self.soup = BeautifulSoup(self.source.content , "lxml")
-         # print soup.prettify()
-
+class SouqScrapper:
+    def __init__(self, url):
+        self.source = requests.get(url)
+        self.soup = BeautifulSoup(self.source.content, "lxml")
+        # print soup.prettify()
 
     def name(self):
-         pn = self.soup.find("div", {"class":"small-12 columns product-title"})
-         pText = pn.find("h1").text
-         return pText
+        pn = self.soup.find("div", {"class": "small-12 columns product-title"})
+        product_name = pn.find("h1").text
+        return product_name
 
     def price(self):
-        price = self.soup.find("div", {"class":"text-default price-container"})
-        price_header = price.find("h3", {"class":"price is sk-clr1"})
-        pValue = price.find("h3").text
-        return pValue
-
+        price = self.soup.find("div", {"class": "text-default price-container"})
+        # price_header = price.find("h3", {"class": "price is sk-clr1"})
+        price_value = price.find("h3").text
+        return price_value
 
     def description(self):
-        description = self.soup.find("div", {"class":"item-details-mini"})
+        description = self.soup.find("div", {"class": "item-details-mini"})
         desc = description.find("p").text
         return desc
 
-
     def image_url(self):
-        image = self.soup.find("div", {"class":"img-bucket"})
-        imgUrl = image.find('img').get('src')
-        return imgUrl
-
-
+        image = self.soup.find("div", {"class": "img-bucket"})
+        image_url = image.find('img').get('src')
+        return image_url
